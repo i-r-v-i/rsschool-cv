756965490008180c6e6a11f62f6811e14c1acefd
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-11-13 16:56:19 +0300

    feat: add webpack.ProgressPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -8,5 +10,10 @@ module.exports = {
         filename: '[name].[contenthash].js',
         clean: true,
     },
-   
+   plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),}
+    ),
+    new webpack.ProgressPlugin(),
+   ]
 }
